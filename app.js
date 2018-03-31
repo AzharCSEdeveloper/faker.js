d0083b473d6a1fc00eb2d7b840ce9e3d58f6f2ae
mr coder <arbis@gmail.com>
2018-04-01 01:01:11 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,12 @@
+var app = document.querySelector('#app');
+app.data= generateContacts(); 
+function generateContacts(){
+  var data=[];
+  for(i=0;i<1000;i++){
+      data.push({
+          name:faker.name.findName(),
+          avatar:faker.internet.avatar(),
+      });
+  } 
+  return data; 
+}
